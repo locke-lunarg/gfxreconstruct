827e4de51b9f144059dee44f8c9a6b96b07ddbf8
Locke Lin <locke@lunarg.com>
2025-09-30 23:24:08 -0600

    Update vulkan_rebind_allocator.cpp

--- framework/decode/vulkan_rebind_allocator.cpp
+++ framework/decode/vulkan_rebind_allocator.cpp
@@ -1032,13 +1032,13 @@ VkResult VulkanRebindAllocator::BindImageMemory(VkImage                         
             {
                 GFXRECON_ASSERT(vma_mem_info);
 
+                auto offset = GetRebindOffsetFromVMA(memory_offset, *vma_mem_info);
+
                 if (resource_alloc_info->capture_id == 99)
                 {
-                    int i = 0;
-                    ++i;
+                    GFXRECON_LOG_WARNING("offset:  %" PRIu64 ",  %" PRIu64 ",  %" PRIu64 ",  %" PRIu64 "", offset, memory_offset, 
+                        vma_mem_info->offset_from_original_device_memory, vma_mem_info->allocation_info.offset);
                 }
-
-                auto offset = GetRebindOffsetFromVMA(memory_offset, *vma_mem_info);
 
                 result = vmaBindImageMemory2(allocator_, vma_mem_info->allocation, offset, image, nullptr);
 
