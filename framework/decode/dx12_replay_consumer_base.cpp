f0b0683b4bd9584b0c9a7b1dc47551a91e8fb6a6
Locke Lin <locke@lunarg.com>
2026-03-04 16:59:04 -0700

    Revert "Update dx12_replay_consumer_base.cpp"
    This reverts commit 60f1e0bcaf31d95c47a10ce6d20f652cf7611693.

--- framework/decode/dx12_replay_consumer_base.cpp
+++ framework/decode/dx12_replay_consumer_base.cpp
@@ -1828,15 +1828,9 @@ HRESULT Dx12ReplayConsumerBase::OverrideCreatePlacedResource(
     auto replay_object = static_cast<ID3D12Device*>(replay_object_info->object);
     auto heap          = static_cast<ID3D12Heap*>(pHeap->object);
 
-    D3D12_RESOURCE_DESC modified_desc = *pDesc->GetPointer();
-    if ((modified_desc.Flags & D3D12_RESOURCE_FLAG_USE_TIGHT_ALIGNMENT) == D3D12_RESOURCE_FLAG_USE_TIGHT_ALIGNMENT)
-    {
-        modified_desc.Alignment = 0;
-    }
-
     auto replay_result = replay_object->CreatePlacedResource(heap,
                                                              HeapOffset,
-                                                             &modified_desc,
+                                                             pDesc->GetPointer(),
                                                              InitialState,
                                                              pOptimizedClearValue->GetPointer(),
                                                              *riid.decoded_value,
