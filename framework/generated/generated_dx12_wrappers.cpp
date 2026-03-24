ef081da669e1dff7e9980766551634ad68e16e32
Locke Lin <locke@lunarg.com>
2026-03-24 15:20:34 -0600

    Update generated_dx12_wrappers.cpp

--- framework/generated/generated_dx12_wrappers.cpp
+++ framework/generated/generated_dx12_wrappers.cpp
@@ -9062,6 +9062,13 @@ D3D12_RESOURCE_ALLOCATION_INFO STDMETHODCALLTYPE ID3D12Device_Wrapper::GetResour
             numResourceDescs,
             pResourceDescs);
 
+        if(pResourceDescs[0].Dimension == D3D12_RESOURCE_DIMENSION_BUFFER && 
+            ((pResourceDescs[0].Flags & D3D12_RESOURCE_FLAG_USE_TIGHT_ALIGNMENT) == D3D12_RESOURCE_FLAG_USE_TIGHT_ALIGNMENT))
+        {
+            // result.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+            result.Alignment = util::platform::GetSystemPageSize();
+        }
+
         Encode_ID3D12Device_GetResourceAllocationInfo(
             this,
             result,
